880ba933cd2e5159a260f22b91ebceaf85ee5ddf
Zsolti <dev@zsoltkaszas.com>
2021-11-22 22:46:52 +0100

    LocalDate -> LocalDateTime

--- src/main/java/hu/webuni/hrholiday/szabi/repository/specification/Holiday2Specification.java
+++ src/main/java/hu/webuni/hrholiday/szabi/repository/specification/Holiday2Specification.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.criteria.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
@@ -21,23 +23,26 @@ public class Holiday2Specification implements Specification<HolidayRequest> {
     public javax.persistence.criteria.Predicate toPredicate(Root<HolidayRequest> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
 
         List<Predicate> predicates = new LinkedList<>();
-      //  Join<HolidayRequest, Employee> requestEmployeeJoin = root.join(HolidayRequest_.employeeCreator, JoinType.LEFT);
-      //  root.fetch(HolidayRequest_.employeeCreator, JoinType.LEFT);
+        //  Join<HolidayRequest, Employee> requestEmployeeJoin = root.join(HolidayRequest_.employeeCreator, JoinType.LEFT);
+        //  root.fetch(HolidayRequest_.employeeCreator, JoinType.LEFT);
 
         if (holidayRequestQuery.getHolidayRequestStatus() != null) {
             predicates.add(criteriaBuilder.equal(root.get(HolidayRequest_.holidayRequestStatus), holidayRequestQuery.getHolidayRequestStatus()));
         }
 
-        if (StringUtils.hasText(holidayRequestQuery.getAcceptorName())){
-            predicates.add(criteriaBuilder.like(root.get(HolidayRequest_.acceptor).get(Boss_.employeeName), holidayRequestQuery.getAcceptorName()+"%"));
+        if (StringUtils.hasText(holidayRequestQuery.getAcceptorName())) {
+            predicates.add(criteriaBuilder.like(root.get(HolidayRequest_.acceptor).get(Boss_.employeeName), holidayRequestQuery.getAcceptorName() + "%"));
         }
 
-        if (StringUtils.hasText(holidayRequestQuery.getRequestorName())){
-            predicates.add(criteriaBuilder.like(root.get(HolidayRequest_.employeeCreator).get(Employee_.employeeName), holidayRequestQuery.getRequestorName()+"%"));
+        if (StringUtils.hasText(holidayRequestQuery.getRequestorName())) {
+            predicates.add(criteriaBuilder.like(root.get(HolidayRequest_.employeeCreator).get(Employee_.employeeName), holidayRequestQuery.getRequestorName() + "%"));
         }
 
-        if (Objects.nonNull(holidayRequestQuery.getCreation_from())){
-            predicates.add(criteriaBuilder.between(root.get(HolidayRequest_.creationTimestamp), holidayRequestQuery.getCreation_from(), holidayRequestQuery.getCreation_to());
+        if (Objects.nonNull(holidayRequestQuery.getCreation_from())) {
+
+            predicates.add(criteriaBuilder.between(root.get(HolidayRequest_.creationTimestamp),
+                    LocalDateTime.of(holidayRequestQuery.getCreation_from(), LocalTime.MIN),
+                    LocalDateTime.of(holidayRequestQuery.getCreation_to(), LocalTime.MAX)));
 
         }
 
